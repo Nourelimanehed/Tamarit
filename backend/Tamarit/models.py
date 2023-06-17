f4d5626396af422252fbe8a95b0d47cccbc7fb0b
mouna lebazda <58882110+mounalebazda@users.noreply.github.com>
2023-06-17 21:55:50 +0100

    Update models.py

--- backend/Tamarit/models.py
+++ backend/Tamarit/models.py
@@ -126,3 +126,52 @@ class Favorite(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class User(AbstractUser):
+    USER_ROLE_CHOICES = (
+        ('touriste', 'Touriste'),
+        ('interface_regional', 'Interface Regional'),
+        ('interface_admin_central', 'Interface Admin Central'),
+    )
+
+    role = models.CharField(max_length=50, choices=USER_ROLE_CHOICES)
+    
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_name='myapp_user_set',  
+        related_query_name='user'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name='myapp_user_set', 
+        related_query_name='user'
+    )
+
+    class Meta(AbstractUser.Meta):
+        pass
+    
+User = get_user_model()
+
+class Profile(models.Model):
+    USER_ROLE_CHOICES = (
+        ('tourist', 'Tourist'),
+        ('interface_regional', 'Interface Regional'),
+        ('interface_admin_central', 'Interface Admin Central'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nom = models.TextField(max_length=500, blank=True)
+    prenom = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True)
+    role = models.CharField(max_length=50, choices=USER_ROLE_CHOICES)
+
+    def __str__(self):
+        return self.user.username
