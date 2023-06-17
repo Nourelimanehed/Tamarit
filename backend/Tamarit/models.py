45e20e965ad900bd5f190113a5c9ac5e9067cd96
mouna lebazda <58882110+mounalebazda@users.noreply.github.com>
2023-06-17 21:48:24 +0100

    Update models.py

--- backend/Tamarit/models.py
+++ backend/Tamarit/models.py
@@ -110,3 +110,19 @@ class ContactMessage(models.Model):
 
     def __str__(self):
         return f"Message from {self.name} ({self.email})"
+
+class Comment(models.Model):
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments_as_author',default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments_as_user')
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.site.name}"
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
