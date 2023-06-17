09336b871dbfa5d1b515c916fb3abd9e905469d6
mouna lebazda <58882110+mounalebazda@users.noreply.github.com>
2023-06-17 13:52:10 +0100

    Update admin.py

--- backend/Tamarit/admin.py
+++ backend/Tamarit/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import *
-#----------------
+
+from .models import Profile
+from .models import Comment
+from .models import Site
+from .models import Region
+
+admin.site.register(Profile)
+admin.site.register(Comment)
 admin.site.register(Site)
-admin.site.register(OpeningHours)
-admin.site.register(Transportation)
-admin.site.register(Images)
-admin.site.register(Event)
-admin.site.register(ContactMessage)
+admin.site.register(Region)
